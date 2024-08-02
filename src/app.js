991c4344f49742d535a810792df32605e0e0b5dd
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-08-02 20:43:31 +0000

    Proyect finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,69 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardSymbols = ["♦", "♥", "♠", "♣"];
+  let cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardColors = ["red", "black"];
+
+  let randomSymbol = Math.floor(Math.random() * cardSymbols.length);
+  let randomNumber = Math.floor(Math.random() * cardNumbers.length);
+  document.querySelector(".top").innerHTML = cardSymbols[randomSymbol];
+  document.querySelector(".bottom").innerHTML = cardSymbols[randomSymbol];
+  document.querySelector(".center").innerHTML = cardNumbers[randomNumber];
 };
+
+const generateButton = document.querySelector(".generateButton");
+const activateTimerButtom = document.querySelector(".activateTimmerButtom");
+const timerElement = document.getElementById("timer");
+const widthCard = document.getElementById("widthCard");
+const heightCard = document.getElementById("heightCard");
+const card = document.getElementById("card");
+const changeSizeButton = document.getElementById("change-card-size");
+
+//actualizar carta al dar click
+generateButton.addEventListener("click", () => {
+  window.onload();
+});
+
+//temporizador
+let timmerInterval;
+let seconds = 0;
+
+activateTimerButtom.addEventListener("click", () => {
+  if (timmerInterval) {
+    clearInterval(timmerInterval);
+  }
+  seconds = 0;
+  timerElement.textContent = `Tiempo: ${seconds} segundos`;
+  timmerInterval = setInterval(() => {
+    seconds++;
+    timerElement.textContent = `Tiempo: ${seconds} segundos`;
+    seconds === 10 ? (clearInterval(timmerInterval), window.onload()) : null;
+  }, 1000);
+});
+
+//cambiar tamaño de la carta
+changeSizeButton.addEventListener("click", () => {
+  const width = widthCard.value;
+  const height = heightCard.value;
+  console.log(width);
+  console.log(height);
+  width ? (card.style.width = width + "px") : null;
+  height ? (card.style.height = height + "px") : null;
+});
